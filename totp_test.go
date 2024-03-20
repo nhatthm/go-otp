28107ea8c61c3a2aa1f69226e5ec8ee7fcf7dc64
nhatthm <thmnhat@gmail.com>
2024-03-20 14:01:40 +0100

    Chain TOTP Secret Providers

--- totp_test.go
+++ totp_test.go
@@ -42,9 +42,20 @@ func TestTOTPSecret_String(t *testing.T) {
 func TestTOTPSecret_TOTPSecret(t *testing.T) {
 	t.Parallel()
 
+	ctx := context.Background()
 	p := otp.TOTPSecret("secret")
 
-	assert.Equal(t, "secret", string(p.TOTPSecret(context.Background())))
+	assert.Equal(t, "secret", string(p.TOTPSecret(ctx)))
+
+	err := p.SetTOTPSecret(ctx, "changed")
+	require.ErrorIs(t, err, otp.ErrTOTPSecretReadOnly)
+
+	assert.Equal(t, "secret", string(p.TOTPSecret(ctx)))
+
+	err = p.DeleteTOTPSecret(ctx)
+	require.ErrorIs(t, err, otp.ErrTOTPSecretReadOnly)
+
+	assert.Equal(t, "secret", string(p.TOTPSecret(ctx)))
 }
 
 func TestNoTOTPSecret(t *testing.T) {
@@ -93,6 +104,182 @@ func TestChainTOTPSecretGetters_NoSecret(t *testing.T) {
 	actual := p.TOTPSecret(context.Background())
 
 	assert.Equal(t, otp.NoTOTPSecret, actual)
+}
+
+func TestChainTOTPSecretProviders_HasSecret(t *testing.T) {
+	t.Parallel()
+
+	p := otp.ChainTOTPSecretProviders(
+		otp.NoTOTPSecret,
+		otp.ChainTOTPSecretProviders(
+			nil,
+			otp.TOTPSecret("secret"),
+		),
+	)
+
+	assert.Equal(t, "secret", string(p.TOTPSecret(context.Background())))
+}
+
+func TestChainTOTPSecretProviders_NoSecret(t *testing.T) {
+	t.Parallel()
+
+	p := otp.ChainTOTPSecretProviders()
+	actual := p.TOTPSecret(context.Background())
+
+	assert.Equal(t, otp.NoTOTPSecret, actual)
+}
+
+func TestChainTOTPSecretProviders_SetSecret(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		scenario      string
+		mockProvider  mock.TOTPSecretProviderMocker
+		expectedError string
+	}{
+		{
+			scenario: "failed",
+			mockProvider: mock.MockTOTPSecretProvider(func(p *mock.TOTPSecretProvider) {
+				p.On("SetTOTPSecret", mock.Anything, otp.TOTPSecret("secret")).
+					Return(assert.AnError)
+			}),
+			expectedError: `assert.AnError general error for testing`,
+		},
+		{
+			scenario: "readonly",
+			mockProvider: mock.MockTOTPSecretProvider(func(p *mock.TOTPSecretProvider) {
+				p.On("SetTOTPSecret", mock.Anything, otp.TOTPSecret("secret")).
+					Return(otp.ErrTOTPSecretReadOnly)
+			}),
+			expectedError: `totp secret is read-only`,
+		},
+		{
+			scenario: "success",
+			mockProvider: mock.MockTOTPSecretProvider(func(p *mock.TOTPSecretProvider) {
+				p.On("SetTOTPSecret", mock.Anything, otp.TOTPSecret("secret")).
+					Return(nil)
+			}),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.scenario, func(t *testing.T) {
+			t.Parallel()
+
+			p := otp.ChainTOTPSecretProviders(tc.mockProvider(t))
+
+			err := p.SetTOTPSecret(context.Background(), "secret")
+
+			if tc.expectedError == "" {
+				require.NoError(t, err)
+			} else {
+				require.EqualError(t, err, tc.expectedError)
+			}
+		})
+	}
+}
+
+func TestChainTOTPSecretProviders_DeleteSecret(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		scenario      string
+		mockProvider  mock.TOTPSecretProviderMocker
+		expectedError string
+	}{
+		{
+			scenario: "failed",
+			mockProvider: mock.MockTOTPSecretProvider(func(p *mock.TOTPSecretProvider) {
+				p.On("DeleteTOTPSecret", mock.Anything).
+					Return(assert.AnError)
+			}),
+			expectedError: `assert.AnError general error for testing`,
+		},
+		{
+			scenario: "readonly",
+			mockProvider: mock.MockTOTPSecretProvider(func(p *mock.TOTPSecretProvider) {
+				p.On("DeleteTOTPSecret", mock.Anything).
+					Return(otp.ErrTOTPSecretReadOnly)
+			}),
+			expectedError: `totp secret is read-only`,
+		},
+		{
+			scenario: "success",
+			mockProvider: mock.MockTOTPSecretProvider(func(p *mock.TOTPSecretProvider) {
+				p.On("DeleteTOTPSecret", mock.Anything).
+					Return(nil)
+			}),
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.scenario, func(t *testing.T) {
+			t.Parallel()
+
+			p := otp.ChainTOTPSecretProviders(tc.mockProvider(t))
+
+			err := p.DeleteTOTPSecret(context.Background())
+
+			if tc.expectedError == "" {
+				require.NoError(t, err)
+			} else {
+				require.EqualError(t, err, tc.expectedError)
+			}
+		})
+	}
+}
+
+func TestChainTOTPSecretProviders(t *testing.T) { //nolint: paralleltest
+	t.Setenv(t.Name(), "env")
+
+	providers := otp.TOTPSecretProviders{
+		otp.NoTOTPSecret,
+		otp.TOTPSecret("secret"),
+		otp.TOTPSecretFromEnv(t.Name()),
+	}
+
+	ctx := context.Background()
+	p := otp.ChainTOTPSecretProviders(providers...)
+
+	assert.Equal(t, "secret", string(p.TOTPSecret(ctx)))
+
+	err := p.SetTOTPSecret(ctx, "changed")
+	require.NoError(t, err)
+
+	// Change secret.
+	assert.Equal(t, "changed", string(p.TOTPSecret(ctx)))
+
+	actualSecrets := make([]otp.TOTPSecret, len(providers))
+	expectedSecrets := []otp.TOTPSecret{"", "secret", "changed"}
+	surrogateSecrets := make([]otp.TOTPSecret, len(providers))
+	expectedSurrogateSecrets := []otp.TOTPSecret{"changed", "changed", "changed"}
+
+	for i := 0; i < len(providers); i++ {
+		actualSecrets[i] = providers[i].TOTPSecret(ctx)
+		surrogateSecrets[i] = p[i].TOTPSecret(ctx)
+	}
+
+	assert.Equal(t, expectedSecrets, actualSecrets)
+	assert.Equal(t, expectedSurrogateSecrets, surrogateSecrets)
+
+	// Delete secret.
+	err = p.DeleteTOTPSecret(ctx)
+	require.NoError(t, err)
+
+	actualSecrets = make([]otp.TOTPSecret, len(providers))
+	expectedSecrets = []otp.TOTPSecret{"", "secret", ""}
+	surrogateSecrets = make([]otp.TOTPSecret, len(providers))
+	expectedSurrogateSecrets = []otp.TOTPSecret{"", "", ""}
+
+	for i := 0; i < len(providers); i++ {
+		actualSecrets[i] = providers[i].TOTPSecret(ctx)
+		surrogateSecrets[i] = p[i].TOTPSecret(ctx)
+	}
+
+	assert.Equal(t, expectedSecrets, actualSecrets)
+	assert.Equal(t, expectedSurrogateSecrets, surrogateSecrets)
 }
 
 func TestTOTPGenerator_GenerateOTP(t *testing.T) {
